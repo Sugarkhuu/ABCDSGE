af5242e612633af76cc138df77fa2633dab4bd07
mcreel <michael@pelican.uab.es>
2016-03-08 12:17:48 +0100

    snapshot before changing param dependent moments integration

--- AIS/DSGE_montecarlo.m
+++ AIS/DSGE_montecarlo.m
@@ -15,7 +15,7 @@ particlequantile = 20; % keep the top % of particles
 verbose = false;
 
 % controls for drawing the final sample from mixture of AIS and prior
-mixture = 0.2; % proportion sampled from original prior 
+mixture = 0.1; % proportion sampled from original prior 
 AISdraws = nworkers*round(5000/nworkers); # number of draws from final AIS density
 
 % controls for the nonparametric fits
@@ -61,6 +61,7 @@ if !node
 	cilower = zeros(mc_reps, nparams);
 	ciupper = zeros(mc_reps, nparams);
 endif
+
 
 for rep = 1:mc_reps
     % the 'true' Zn
@@ -116,8 +117,8 @@ for rep = 1:mc_reps
         endfor      
         % selected bws from tuning
         % selected using prior
-        bwselect = [5    8    7   11   30    9   13    7   11]; % for LL
-        bwselectCI = [  22   17   16   13   21   22   16   12    7]; % for LC CIs
+        bwselect = [ 8   16   16   10   11   12   17   20   19]; % for LL
+        bwselectCI = [  5    7   24    8   30   27   27    6   25]; % for LC CIs
         
         % selected using local
         
@@ -132,64 +133,6 @@ for rep = 1:mc_reps
         thetas = thetas(test,:);
         Zs = Zs(test,:);
         Z = [Zn; Zs];
-
-function pdm = makepdm(thetas, realdata)        
-        % add the parameter-dependent moments
-        y = realdata(:,1);
-        c = realdata(:,2);
-        n = realdata(:,3);
-        r = realdata(:,4);
-        w = realdata(:,5);
-        pdm = zeros(rows(thetas),6);
-        for i = 1:rows(thetas)
-            % break out params    
-            alpha = thetas(i,1);
-            beta = thetas(i,2);
-            delta = thetas(i,3);
-            gam = thetas(i,4);
-            rho_z = thetas(i,5);
-            sig_z = thetas(i,6);
-            rho_eta = thetas(i,7);
-            sig_eta = thetas(i,8);
-            nss = thetas(i,9);
-            % recover psi
-            c1 = ((1/beta + delta - 1)/alpha)^(1/(1-alpha));
-            kss = nss/c1;
-            iss = delta*kss;
-            yss = kss^alpha * nss^(1-alpha);
-            css = yss - iss;
-            psi =  (css^(-gam)) * (1-alpha) * (kss^alpha) * (nss^(-alpha));
-            % get white noise shock to prefs
-            e = log(w)-rho_eta*lag(log(w),1) - gam*(log(c) - rho_eta*lag(log(c),1))-log(psi)*(1-rho_eta);
-            e = e/sig_eta;
-            e = e(3:end,:);
-            pdm(i,1) = mean(e);
-            pdm(i,2) = mean(e.^2 - 1);
-            %pdm(i,3) = mean(e(1:end-1,:).*e(2:end,:));
-            %e1 = e;
-            % now the Euler eqn
-            cc = c ./lag(c,1);
-            e = beta*cc.^(-gam).*(1 + r -delta)-1;
-            e = e(2:end,:);
-            pdm(i,3) = mean(e);
-            % production function
-            k = alpha*lag(n,1).*lag(w,1)./lag(r,1)/(1-alpha);
-            lnz = log(y) - alpha*log(k) - (1-alpha)*log(n);
-            e = lnz - rho_z*lag(lnz,1);
-            e = e(3:end,:);
-            e = e/sig_z;
-            pdm(i,4) = mean(e);
-            pdm(i,5) = mean(e.^2 - 1);
-            %pdm(i,6) = mean(e(1:end-1,:).*e(2:end,:));
-            %pdm(i,7) = mean(e.*e1); % no cross correlation between errors
-            % law of motion k: good for delta
-            invest = y - c;
-            e = lag(invest,1) + (1 - delta)*lag(k,1) - k;
-            e = e(3:end,:);
-            pdm(i,6) = mean(e);
-        endfor
-        pdm = [zeros(1,size(pdm,2)); pdm]; % stack real data stats (zeros) on top
-endfunction
 
         pdm = makepdm(thetas, realdata);
         Z = [Z pdm]; 
